a4fec672d4a04969b2737197b33cef82a33b92a3
negashwe <negashsoft@hotmail.com>
2021-04-17 01:06:55 -0400

    file

---
+++ negashfriday/src/negashfriday/negashclass.java
@@ -0,0 +1,5 @@
+package negashfriday;
+
+public class negashclass {
+
+}
---
+++ negashfriday/src/negashfriday/negashproject.java
@@ -0,0 +1,11 @@
+package negashfriday;
+
+public class negashproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello class");
+
+	}
+
+}
